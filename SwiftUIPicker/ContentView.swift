6d19a6e6606bdbd4113887706dec13f7a2ee7c73
Sophia <gmb728@gmail.com>
2020-04-23 15:29:45 +0800

    Initial Commit

---
+++ SwiftUIPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPicker
+//
+//  Created by Chang Sophia on 4/23/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
